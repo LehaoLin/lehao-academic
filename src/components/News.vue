1d4c8f2ee916493ddae6783e9190b9deaa9c98ad
LehaoLin <lehaolin@link.cuhk.edu.cn>
2024-08-06 15:47:32 +0800

    update

--- src/components/News.vue
+++ src/components/News.vue
@@ -2,6 +2,9 @@
   <h1>News</h1>
 
   <el-timeline>
+    <el-timeline-item center timestamp="2024-8" placement="top">
+      Become a Ph.D. candidate.
+    </el-timeline-item>
     <el-timeline-item center timestamp="2024-7" placement="top">
       One technical paper was accepted by ACM MM'24.
     </el-timeline-item>
