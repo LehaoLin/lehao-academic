046059730ff50ac0e39556206846ee7aa68f06b5
LehaoLin <lehaolin@link.cuhk.edu.cn>
2024-08-17 23:44:02 +0800

    update

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -36,19 +36,31 @@
 
     <el-row>
       <el-text size="default"
-        >ACM International Symposium on Blockchain and Secure Critical
-        Infrastructure 2023 (BSCI'23)</el-text
+        >ACM International Workshop on Network and Operating System Support for
+        Digital Audio and Video 2022, 2023, 2024 (NOSSDAV 2022, 2023,
+        2024)</el-text
       >
     </el-row>
     <el-row>
       <el-text size="default"
-        >ACM International Conference on Multimedia 2022 (ACM MM 2022)
+        >ACM International Conference on Multimedia 2022, 2024(ACM MM 2022,
+        2024)
       </el-text>
     </el-row>
     <el-row>
       <el-text size="default"
-        >ACM International Workshop on Network and Operating System Support for
-        Digital Audio and Video 2022 (NOSSDAV 2022)
+        >ACM Conference on Human Factors in Computing Systems 2024 (CHI 2024)
+      </el-text>
+    </el-row>
+    <el-row>
+      <el-text size="default"
+        >ACM International Symposium of Chinese CHI 2023 (CHCHI 2023)
+      </el-text>
+    </el-row>
+    <el-row>
+      <el-text size="default"
+        >ACM International Symposium on Blockchain and Secure Critical
+        Infrastructure 2023 (BSCI 2023)
       </el-text>
     </el-row>
   </el-card>
