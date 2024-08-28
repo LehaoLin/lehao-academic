d68bde66abcca72d9a9393e37196a2048b0a39b4
LehaoLin <lehaolin@link.cuhk.edu.cn>
2024-08-29 03:51:48 +0800

    update

--- src/components/News.vue
+++ src/components/News.vue
@@ -8,9 +8,6 @@
     <el-timeline-item center timestamp="2024-7" placement="top">
       One technical paper was accepted by ACM MM'24.
     </el-timeline-item>
-    <el-timeline-item center timestamp="2023-9" placement="top">
-      Start my Teaching Assistant for MAT2040: Linear Algebra in CUHK(SZ).
-    </el-timeline-item>
     <el-timeline-item timestamp="2023-9" placement="top">
       Start my new personal webpage 💐.
     </el-timeline-item>
