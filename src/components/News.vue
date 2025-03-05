686d40cd0029d5e9fb86dfafe80d28dfb73a4c3f
LehaoLin <lehaolin@link.cuhk.edu.cn>
2025-03-06 00:36:51 +0800

    update

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,7 +1,7 @@
 <template>
   <h1>News</h1>
 
-  <el-timeline>
+  <!-- <el-timeline>
     <el-timeline-item center timestamp="2025-2" placement="top">
       One paper was accepted by ACM CHI'25 Late Breaking Work (LBW).
     </el-timeline-item>
@@ -17,9 +17,73 @@
     <el-timeline-item timestamp="2023-9" placement="top">
       Start my new personal webpage 💐.
     </el-timeline-item>
+    <el-timeline-item>
+      <el-link type="primary">Show More</el-link>
+      <el-link type="primary">Show Less</el-link>
+    </el-timeline-item>
+  </el-timeline> -->
+
+  <el-timeline>
+    <el-timeline-item
+      v-for="item in news.slice(0, 6)"
+      v-if="expand == false"
+      center
+      :timestamp="item.timestamp"
+      placement="top"
+    >
+      {{ item.content }}
+    </el-timeline-item>
+
+    <el-timeline-item
+      v-if="expand == true"
+      v-for="item in news"
+      center
+      :timestamp="item.timestamp"
+      placement="top"
+    >
+      {{ item.content }}
+    </el-timeline-item>
+
+    <el-timeline-item v-if="news.length > 6">
+      <el-link v-if="expand == false" type="primary" @click="show_more()"
+        >Show More</el-link
+      >
+      <el-link v-if="expand == true" type="primary" @click="show_less()"
+        >Show Less</el-link
+      >
+    </el-timeline-item>
   </el-timeline>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const expand = ref(false);
+
+const news = ref([
+  {
+    timestamp: "2025-2",
+    content: "One paper was accepted by ACM CHI'25 Late Breaking Work (LBW).",
+  },
+  {
+    timestamp: "2025-1",
+    content: "One paper was accepted by ACM CHI'25.",
+  },
+  { timestamp: "2024-8", content: "Become a Ph.D. candidate." },
+  {
+    timestamp: "2024-7",
+    content: "One technical paper was accepted by ACM MM'24.",
+  },
+  { timestamp: "2023-9", content: "Start my new personal webpage 💐." },
+]);
+
+const show_more = () => {
+  expand.value = true;
+};
+
+const show_less = () => {
+  expand.value = false;
+};
+</script>
 
 <style scoped></style>
