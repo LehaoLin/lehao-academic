5bca4686d7b9768bf10b3dc78c24e2977164d3f8
LehaoLin <lehaolin@link.cuhk.edu.cn>
2025-03-13 16:43:13 +0800

    update

--- src/components/News.vue
+++ src/components/News.vue
@@ -24,34 +24,36 @@
   </el-timeline> -->
 
   <el-timeline>
-    <el-timeline-item
-      v-for="item in news.slice(0, 6)"
-      v-if="expand == false"
-      center
-      :timestamp="item.timestamp"
-      placement="top"
-    >
-      {{ item.content }}
-    </el-timeline-item>
-
-    <el-timeline-item
-      v-if="expand == true"
-      v-for="item in news"
-      center
-      :timestamp="item.timestamp"
-      placement="top"
-    >
-      {{ item.content }}
-    </el-timeline-item>
-
-    <el-timeline-item v-if="news.length > 6">
-      <el-link v-if="expand == false" type="primary" @click="show_more()"
-        >Show More</el-link
+    <el-scrollbar height="20vh" always>
+      <el-timeline-item
+        v-for="item in news.slice(0, 6)"
+        v-if="expand == false"
+        center
+        :timestamp="item.timestamp"
+        placement="top"
       >
-      <el-link v-if="expand == true" type="primary" @click="show_less()"
-        >Show Less</el-link
+        {{ item.content }}
+      </el-timeline-item>
+
+      <el-timeline-item
+        v-if="expand == true"
+        v-for="item in news"
+        center
+        :timestamp="item.timestamp"
+        placement="top"
       >
-    </el-timeline-item>
+        {{ item.content }}
+      </el-timeline-item>
+
+      <el-timeline-item v-if="news.length > 6">
+        <el-link v-if="expand == false" type="primary" @click="show_more()"
+          >Show More</el-link
+        >
+        <el-link v-if="expand == true" type="primary" @click="show_less()"
+          >Show Less</el-link
+        >
+      </el-timeline-item>
+    </el-scrollbar>
   </el-timeline>
 </template>
 
@@ -62,12 +64,13 @@ const expand = ref(false);
 
 const news = ref([
   {
-    timestamp: "2025-2",
-    content: "One paper was accepted by ACM CHI'25 Late Breaking Work (LBW).",
+    timestamp: "2025-3",
+    content: "One paper was accepted by ACM TOMM.",
   },
   {
     timestamp: "2025-1",
-    content: "One paper was accepted by ACM CHI'25.",
+    content:
+      "Two papers were accepted by ACM CHI'25 and Late Breaking Work (LBW).",
   },
   { timestamp: "2024-8", content: "Become a Ph.D. candidate." },
   {
