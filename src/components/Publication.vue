344b4ce5fbf82a9fa24d1728ed94b8893ff67663
LehaoLin <lehaolin@link.cuhk.edu.cn>
2025-03-22 02:11:52 +0800

    update

--- src/components/Publication.vue
+++ src/components/Publication.vue
@@ -19,8 +19,8 @@
     <h3>2025</h3>
     <div>
       <el-tag type="warning" effect="dark">CCF-B</el-tag>
-      [10] <u>Lehao Lin</u>, Hong Kang, Yuqi Shi, Haihan Duan, Abdulmotaleb El
-      Saddik, and Wei Cai†.
+      [10] <b><u>Lehao Lin</u></b
+      >, Hong Kang, Yuqi Shi, Haihan Duan, Abdulmotaleb El Saddik, and Wei Cai†.
       <i
         >ASimp: Automatic High-Poly 3D Mesh Simplification for Preprocessing
         Based on QoE</i
@@ -30,7 +30,8 @@
     <br />
     <div>
       <el-tag type="warning" effect="dark">CCF-B</el-tag>
-      [9] Hao Wu, Maha Abdallah, Yuanfang Chi, <u>Lehao Lin</u>, and Wei Cai†.
+      [9] Hao Wu, Maha Abdallah, Yuanfang Chi, <b><u>Lehao Lin</u></b
+      >, and Wei Cai†.
       <i>Web3 Multimedia Applications: Under the Impact of Decentralization</i>.
       ACM Transactions on Multimedia Computing Communications and Applications
       (TOMM), 2025. CCF-B.
@@ -38,7 +39,8 @@
     <br />
     <div>
       <el-tag type="danger" effect="dark">CCF-A</el-tag>
-      [8] Hao Wu, <u>Lehao Lin</u>, Maha Abdallah, and Wei Cai†.
+      [8] Hao Wu, <b><u>Lehao Lin</u></b
+      >, Maha Abdallah, and Wei Cai†.
       <i>Rethinking Participatory Art under Decentralization in Web</i>. In The
       2025 ACM CHI Conference on Human Factors in Computing Systems Late
       Breaking Work (CHI 2025 LBW), Yokohama, Japan, April 26-May 1, 2025.
@@ -47,7 +49,8 @@
     <br />
     <div>
       <el-tag type="danger" effect="dark">CCF-A</el-tag>
-      [7] Ke Wang*, <u>Lehao Lin</u>*, Maha Abdallah, Wei Cai†.
+      [7] Ke Wang*, <b><u>Lehao Lin</u></b
+      >*, Maha Abdallah, Wei Cai†.
       <i
         >Where is the Boundary? Understanding How People Recognize and Evaluate
         Generative AI-extended Videos</i
@@ -59,7 +62,8 @@
     <h3>2024</h3>
     <div>
       <el-tag type="danger" effect="dark">CCF-A</el-tag>
-      [6] <u>Lehao Lin</u>*, Hong Kang*, Xinyao Sun, Wei Cai†.
+      [6] <b><u>Lehao Lin</u></b
+      >*, Hong Kang*, Xinyao Sun, Wei Cai†.
       <i
         >SemNFT: A Semantically Enhanced Decentralized Middleware for Digital
         Asset Immortality</i
@@ -69,7 +73,8 @@
     <br />
     <div>
       <el-tag type="danger" effect="dark">CCF-A</el-tag>
-      [5] Hong Kang, Minghao Li, <u>Lehao Lin</u>, Sizheng Fan, Wei Cai†.
+      [5] Hong Kang, Minghao Li, <b><u>Lehao Lin</u></b
+      >, Sizheng Fan, Wei Cai†.
       <i
         >Bridging Incentives and Dependencies: An Iterative Combinatorial
         Auction Approach to Dependency-Aware Offloading in Mobile Edge
@@ -80,7 +85,8 @@
     <div>
       <el-tag type="warning" effect="dark">CCF-B</el-tag>
       [4] Haihan Duan, Junhua Liao,
-      <u>Lehao Lin</u>, Abdulumotaleb El Saddik, Wei Cai†.
+      <b><u>Lehao Lin</u></b
+      >, Abdulumotaleb El Saddik, Wei Cai†.
       <i
         >Meetor: A Human-Centered Automatic Video Editing System for Meeting
         Recordings</i
@@ -92,7 +98,8 @@
     <h3>2023</h3>
     <div>
       <el-tag type="info" effect="dark">Non-CCF</el-tag>
-      [3] <u>Lehao Lin</u>, Haihan Duan, Wei Cai†.
+      [3] <b><u>Lehao Lin</u></b
+      >, Haihan Duan, Wei Cai†.
       <i
         >Web3DP: A Crowdsourcing Platform for 3D models Based on Web3
         Infrastructure</i
@@ -105,7 +112,8 @@
     <div>
       <el-tag type="warning" effect="dark">CCF-B</el-tag>
       [2] Haihan Duan, Junhua Liao,
-      <u>Lehao Lin</u>, Wei Cai†.
+      <b><u>Lehao Lin</u></b
+      >, Wei Cai†.
       <i
         >FLAD: A Human-centered Video Content Flaw Detection System for Meeting
         Recordings</i
@@ -118,7 +126,8 @@
     <h3>2021</h3>
     <div>
       <el-tag type="success" effect="dark">CCF-C</el-tag>
-      [1] Lin Wang, <u>Lehao Lin</u>, Xiao Wu, Rongman Hong†.
+      [1] Lin Wang, <b><u>Lehao Lin</u></b
+      >, Xiao Wu, Rongman Hong†.
       <i>Seshat: Decentralizing Oral History Text Analysis</i>. In the 17th
       International Conference on Mobility, Sensing and Networking (MSN 2021),
       Exeter, United Kingdom, December 13-15, 2021. CCF-C.
