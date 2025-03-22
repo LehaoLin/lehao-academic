0f08644eacbb92b52cc4322a322b43ed760610bf
LehaoLin <lehaolin@link.cuhk.edu.cn>
2025-03-23 02:50:20 +0800

    udpate

--- src/components/News.vue
+++ src/components/News.vue
@@ -66,6 +66,10 @@ const expand = ref(false);
 
 const news = ref([
   {
+    timestamp: "2025-3-20",
+    content: "I passed my Ph.D. thesis oral defense.",
+  },
+  {
     timestamp: "2025-3",
     content: "One paper was accepted by ACM TOMM.",
   },
